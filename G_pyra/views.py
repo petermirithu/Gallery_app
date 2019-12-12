031168825964ed2f3426be5a25004c8b5251db52
petermirithu <pyra_m.k@yahoo.com>
2019-12-12 21:40:17 +0300

    add a view function to render home template

--- G_pyra/views.py
+++ G_pyra/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
 
-# Create your views here.
+def home(request):
+  '''
+  view function that renders the home page on application's start up
+  '''  
+  return render(request,'home.html')
+
+
