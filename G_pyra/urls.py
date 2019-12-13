e9856b0e8a40c1102e12cba541a33e0fd27424f0
petermirithu <pyra_m.k@yahoo.com>
2019-12-13 21:08:51 +0300

    define url for search,single image and images

--- G_pyra/urls.py
+++ G_pyra/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url('^$',views.home,name='home'),    
+    url(r'^search/',views.search,name="searchRes"),
+    url(r'^image/(\d+)',views.single_image,name="image"),
+    url(r'^images/(Nairobi)',views.images_by_location,name="ImagesByLocation"),
 ]
 
 if settings.DEBUG:
