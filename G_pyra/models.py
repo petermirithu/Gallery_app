b271cf946aa0dc69881335e50628ead272a0f243
petermirithu <pyra_m.k@yahoo.com>
2019-12-13 12:12:33 +0300

    create models for the category,location and image

--- G_pyra/models.py
+++ G_pyra/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import datetime as dt
 
-# Create your models here.
+class Category(models.Model):
+  '''
+  class that defines the categories for the images
+  '''
+  name=models.CharField(max_length=50)
+
+  def __str__(self):
+      return self.name
+
+class Location(models.Model):
+  '''
+  class that defines Locations where the images were taken
+  '''
+  name=models.CharField(max_length=50)
+
+  def __str__(self):
+      return self.name
+
+class Image(models.Model):
+  '''
+  class that defines how an image is stored in the database
+  '''
+  image_name=models.CharField(max_length=30)
+  image_url=models.ImageField(upload_to='images/',blank=True)
+  description=models.CharField(max_length=500)
+  posted_on=models.DateField(auto_now_add=True)  
+  category=models.ForeignKey(Category,on_delete=models.CASCADE)
+  location=models.ForeignKey(Location,on_delete=models.CASCADE)
+
+  def __str__(self):
+      return self.image_name
+  
+
+
+  
+  
+
