fb5d40a11a0465c71f24fadd0ee58e4fc9ec2cb7
petermirithu <pyra_m.k@yahoo.com>
2019-12-13 12:12:49 +0300

    create url for home page

--- G_pyra/urls.py
+++ G_pyra/urls.py
@@ -4,5 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url('^$',views.home,name='home'),
+    url('^$',views.home,name='home'),    
 ]
+
+if settings.DEBUG:
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
