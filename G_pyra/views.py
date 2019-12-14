9105a8a2f75602dad73218575401fc03482c290c
petermirithu <pyra_m.k@yahoo.com>
2019-12-15 00:37:59 +0300

    add view functions for copying,location and category display

--- G_pyra/views.py
+++ G_pyra/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 from .models import Image,Category,Location
+from django.core.exceptions import ObjectDoesNotExist
+
 def home(request):
   '''
   view function that renders the home page on application's start up
@@ -19,21 +21,9 @@ def search(request):
       return render(request,'search.html',{"message":message,"images":results,"title":title})
 
     else:
-      message="You haven't search for category"  
+      message="You haven't search for a category"  
       title='Results'
       return render(request, 'search.html',{"message":message,"title":title})
-
-def single_image(request,image_id):
-  '''
-  view function that renders a single image with its details on a single page
-  '''
-  try:
-    image=Image.objects.get(id=image_id)
-    title=image.name
-  except DoesNotExist:
-    raise Http404()
-
-  return render(request,'single.html',{"image":image,"title":title})    
 
 def images_by_location(request, location_name):
   '''
@@ -44,9 +34,31 @@ def images_by_location(request, location_name):
     images=Image.get_images_by_location(found_location)    
     title=location_name
     
-  except DoesNotExist:
+  except ObjectDoesNotExist:
     raise Http404()
 
   return render(request, 'images.html',{"images":images,"title":title})
+
+def copy_image_url(request,image_url):
+  '''
+  view function that refreshes the page once someona has copied the image url
+  '''
+  copied_ulr=Image.copy_image(image_url)
+  
+  return render(request,'home.html',{"copied":copied_ulr})
+
+def images_by_category(request, category_name):
+  '''
+  view function that renders all images with same category
+  '''
+  try:
+    found_category=Category.search_category(category_name)
+    images=Image.search_image(found_category)    
+    title=category_name
+    
+  except ObjectDoesNotExist:
+    raise Http404()
+
+  return render(request, 'Bycategory.html',{"images":images,"title":title})
 
 
