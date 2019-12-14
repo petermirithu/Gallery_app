62d0c7a6346cdada12ba4fccfb90b0e62c3b5c4e
petermirithu <pyra_m.k@yahoo.com>
2019-12-15 00:37:19 +0300

    add path for category,location templates

--- G_pyra/urls.py
+++ G_pyra/urls.py
@@ -2,12 +2,14 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from django.urls import path
 
 urlpatterns = [
     url('^$',views.home,name='home'),    
-    url(r'^search/',views.search,name="searchRes"),
-    url(r'^image/(\d+)',views.single_image,name="image"),
-    url(r'^images/(Nairobi)',views.images_by_location,name="ImagesByLocation"),
+    url(r'^search/',views.search,name="searchRes"),    
+    path('images/<str:location_name>',views.images_by_location,name="ImagesByLocation"),
+    path('category/<str:category_name>',views.images_by_category,name="ImagesByCategory"),
+    path('copy/images/<str:image_url>',views.copy_image_url,name="copyUrl"),
 ]
 
 if settings.DEBUG:
